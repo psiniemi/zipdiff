df02b2e219ab3c7f69733eff40f18a0fdd4697c2
Pasi Niemi <pasi.niemi@iki.fi>
2011-10-19 12:38:35 +0300

    Maven3 configuration, SplitZipBuilder to make three files: common, file1 unique and file2 unique

---
+++ src/main/java/zipdiff/util/StreamUtil.java
@@ -0,0 +1,36 @@
+package zipdiff.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamUtil {
+	/**
+	 * copies data from an input stream to an output stream
+	 *
+	 * @param input InputStream
+	 * @param output OutputStream
+	 * @throws IOException in case of an input/output error
+	 */
+	public static void copyStream(InputStream input, OutputStream output) throws IOException {
+		byte buffer[] = new byte[4096];
+		try {
+			int count = input.read(buffer);
+			while (count > -1) {
+				output.write(buffer, 0, count);
+				count = input.read(buffer);
+			}
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				output.flush();
+			} catch (IOException e0) {
+				throw e0;
+			} finally {
+				if (input != null) input.close();
+			}
+		}
+	}
+
+}
